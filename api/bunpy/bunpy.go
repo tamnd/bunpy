f32236b41c14006f54b58873735f03b2528a6177
tamnd <1218621+tamnd@users.noreply.github.com>
2026-04-28 01:53:08 +0700

    v0.6.6: bunpy build --define -- build-time constant substitution

--- api/bunpy/bunpy.go
+++ api/bunpy/bunpy.go
@@ -22,7 +22,7 @@ import (
 )
 
 // Version is baked in by the bunpy build pipeline.
-const Version = "0.6.5"
+const Version = "0.6.6"
 
 // Modules returns the NativeModules map for the current v0.3.1 surface.
 // Later rungs extend this map by adding more entries.
